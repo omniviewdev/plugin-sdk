120c1ff09d5b107c3aff76e3c9f143f3390f8c16
Joshua Pare <joshuabpare@gmail.com>
2026-03-20 15:31:39 -0500

    cleanup: remove dead Values() method from gRPC settings client
    Values() is not part of the Provider interface and is never called.
    Remove it along with the now-unused "fmt" import.

--- pkg/v1/settings/client.go
+++ pkg/v1/settings/client.go
@@ -2,7 +2,6 @@ package settings
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/omniviewdev/plugin-sdk/settings"
 	"google.golang.org/protobuf/types/known/anypb"
@@ -16,26 +15,6 @@ type Client struct {
 }
 
 var _ Provider = (*Client)(nil)
-
-func (c *Client) Values() map[string]any {
-	resp, err := c.client.ListSettings(context.Background(), &emptypb.Empty{})
-	if err != nil {
-		// we should never error here since the receiver never emits one
-		return nil
-	}
-
-	m := make(map[string]any)
-	for k, v := range resp.GetSettings() {
-		value, err := ConvertAnyToInterface(v.GetValue())
-		if err != nil {
-			continue
-		}
-		key := fmt.Sprintf("%s.%s", k, v.GetId())
-		m[key] = value
-	}
-
-	return m
-}
 
 func (c *Client) ListSettings() map[string]settings.Setting {
 	resp, err := c.client.ListSettings(context.Background(), &emptypb.Empty{})
